894352e50795cf1d5263f069de85eb69bdcff382
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-04-27 20:58:45 -0400

    finished mobile version of card

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 "use strict";
+const userContent = document.querySelector(".user-content");
+const shareContent = document.querySelector(".share-content");
+const ShareIconContainer = document.querySelector(".share-icon-container");
+const shareIcon = document.querySelector(".share-icon");
+const cardFooter = document.querySelector(".card-footer");
+
+const app = () => {
+  ShareIconContainer.addEventListener("click", () => {
+    cardFooter.classList.toggle("footer-active");
+    ShareIconContainer.classList.toggle("active-container");
+    shareIcon.classList.toggle("active-icon");
+    shareContent.classList.toggle("hidden");
+    userContent.classList.toggle("hidden");
+  });
+};
+
+app();
